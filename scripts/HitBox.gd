3dceeaef8901e85dbed267d78211805f37242aa5
huxiaoou <516984451@qq.com>
2025-09-14 22:33:15 +0800

    Add hit effects to hitbox

--- scripts/HitBox.gd
+++ scripts/HitBox.gd
@@ -1,8 +1,15 @@
 class_name HitBox extends Area2D
 
 # when a node gets hit, means this node is hurted by other people
+@onready var effect_player: AnimatedSprite2D = $EffectPlayer
+
 
 signal Damaged(damage: float)
 
 func take_damage(damage: float) -> void:
 	Damaged.emit(damage)
+	var sn = randi() % 2
+	if sn == 0:
+		effect_player.play("slash01")
+	elif sn == 1:
+		effect_player.play("slash08")
