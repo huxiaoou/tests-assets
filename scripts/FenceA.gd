45f641d172da532f12774b91f3f11351439a5b3c
huxiaoou <516984451@qq.com>
2025-09-15 22:47:18 +0800

    Add broken sound to FenceA

--- scripts/FenceA.gd
+++ scripts/FenceA.gd
@@ -1,6 +1,8 @@
 class_name FenceA extends Node2D
 
-@export var hp: float = 12
+@export var hp: float = 2
+@export var destructin_sound: AudioStream
+@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	$HitBox.Damaged.connect(take_damage)
@@ -11,4 +13,7 @@ func take_damage(damage: float) -> void:
 	print("fence A take damage: " + str(damage) + ", remaining hp: " + str(hp))
 	$EffectShake.start()
 	if hp <= 1e-4:
-		queue_free()
+		audio.stream = destructin_sound
+		audio.play(0.9)
+		audio.finished.connect(queue_free)
+		$Sprite2D.visible = false
