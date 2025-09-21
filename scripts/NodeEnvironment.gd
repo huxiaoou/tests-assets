8f185a52702b41bce560907e9e287b4a7ac7efcd
huxiaoou <516984451@qq.com>
2025-09-21 20:34:10 +0800

    Add more envs

--- scripts/NodeEnvironment.gd
+++ scripts/NodeEnvironment.gd
@@ -7,18 +7,25 @@ class_name NodeEnvironment extends Node2D
 @onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var hit_box: HitBox = $HitBox
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
 
 func _ready() -> void:
 	hit_box.initialize(animated_sprite_2d)
 	hit_box.Damaged.connect(take_damage)
+	timer.timeout.connect(die)
 
 func take_damage(damage: float) -> void:
 	if not invincible:
 		hp -= damage
-		print("take damage: " + str(damage) + ", remaining hp: " + str(hp))
+		print(str(self.name) + " take damage: " + str(damage) + ", remaining hp: " + str(hp))
 	
 		if hp <= 1e-4:
 			audio.stream = sound_destruction
 			audio.play(0.4)
-			audio.finished.connect(queue_free)
 			animated_sprite_2d.visible = false
+			timer.start()
+	return
+
+func die() -> void:
+	queue_free()
+	return
